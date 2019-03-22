7f0a8f9025647317ca3cc2fb36d29b6160f36aa8
James Clay <jamesphilipclay@hotmail.com>
2019-03-21 20:29:27 -0400

    reworked code

--- oddities.java
+++ oddities.java
@@ -5,13 +5,11 @@ public class Oddities{
 	public static void main(String[] args){
 		int input = new Scanner(System.in);
 		int test = args[0];
-		boolean isEven = false;
 		
 		for(int i = 0;i<test;i++){
-			int checkNumber = input.nextInt();
-			if(checkNumber*test){
+			int checkNumber = args[i];
 
-			}//if
+			boolean isEven = false;
 		}//for
 	}//main
 
