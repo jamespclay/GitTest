9bc400a7c900d615ea2ea2469c949cd5e1cee15e
James Clay <jamesphilipclay@hotmail.com>
2019-03-21 20:35:47 -0400

    added for

--- oddities.java
+++ oddities.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class Oddities{
 
 	public static void main(String[] args){
-		int input = new Scanner(System.in);
 		int test = args[0];
 		
 		for(int i = 0;i<test;i++){
 			int checkNumber = args[i];
 
 			boolean isEven = false;
-		}//for
+
+			for(int j=0;j<checkNumber;j++){
+				
+			}//for j
+		}//for i
 	}//main
 
 }//class
